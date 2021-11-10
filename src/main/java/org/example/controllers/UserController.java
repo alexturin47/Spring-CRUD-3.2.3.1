4695fec5d7b2075ce145aa42d06106cbc1e23d32
Alexander Tyurin <alexturin@gmail.com>
2021-11-10 21:24:50 +0700

    готово

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -2,6 +2,8 @@ package org.example.controllers;
 
 import org.example.dao.UserDao;
 import org.example.model.User;
+import org.example.service.UserService;
+import org.example.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,40 +12,40 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class UserController {
 
-    private final UserDao userDao;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserDao userDao) {
-        this.userDao = userDao;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/")
     public String index(Model model, @ModelAttribute("user") User user) {
-        model.addAttribute("users", userDao.index());
+        model.addAttribute("users", userService.index());
         return "/index";
     }
 
     @PostMapping()
     public String create(@ModelAttribute("user") User user) {
-        userDao.save(user);
+        userService.save(user);
         return "redirect:/";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
-        userDao.delete(id);
+        userService.delete(id);
         return "redirect:/";
     }
 
     @GetMapping("/edit/{id}")
     public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("user", userDao.read(id));
+        model.addAttribute("user", userService.read(id));
         return "/edit";
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
-        userDao.update(id, user);
+        userService.update(id, user);
         return "redirect:/";
     }
 }
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -1,45 +1,14 @@
 package org.example.service;
 
-import org.example.dao.UserDao;
-import org.example.dao.UserDaoEntityManagerImpl;
-//import org.example.dao.UserDaoImpl;
 import org.example.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Component
-public class UserService implements UserServiceInterface{
-
-    @Autowired
-    private UserDao userDao;
-
-    @Transactional(readOnly = true)
-    @Override
-    public List<User> index() {
-        return userDao.index();
-    }
-
-    @Override
-    public void save(User user) {
-        userDao.save(user);
-    }
-
-    @Override
-    public User read(int id) {
-        return userDao.read(id);
-    }
-
-    @Override
-    public void update(int id, User user) {
-        userDao.update(id, user);
-    }
-
-    @Override
-    public void delete(int id) {
-        userDao.delete(id);
-    }
-
+public interface UserService {
+    List<User> index();
+    void save(User user);
+    User read(int id);
+    void update(int id, User user);
+    void delete(int id);
 }
